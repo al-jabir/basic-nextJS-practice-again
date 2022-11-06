c0f1ef0aebd73a0956a44e2aee020ba6736294bd
al-jabir <aljabir098@gmail.com>
2022-11-06 22:25:25 +0600

    route

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const FirstPost = () => {
+  return (
+    <>
+      <Head>
+        <title>Next.js first post</title>
+      </Head>
+      <div>First blog post</div>
+    </>
+  );
+};
+
+export default FirstPost;
