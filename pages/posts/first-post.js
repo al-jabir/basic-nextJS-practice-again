92c063a674d81e42e1757a96e4630cbbf2eec37a
al-jabir <aljabir098@gmail.com>
2022-11-06 22:39:37 +0600

    img

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import React from 'react';
+import pic from '../../public/assets/pic.jpg';
 
 const FirstPost = () => {
   return (
@@ -7,7 +9,20 @@ const FirstPost = () => {
       <Head>
         <title>Next.js first post</title>
       </Head>
-      <div>First blog post</div>
+      <div style={{ textAlign: 'center' }}>
+        <h1>First blog post</h1>
+        <Image
+          src={pic}
+          alt='this is pic'
+          width={300}
+          height={400}
+          style={{
+            border: '1px solid tomato',
+            borderRadius: '1rem',
+            margin: 'auto',
+          }}
+        />
+      </div>
     </>
   );
 };
