709bcd87bf384a7fdd468bcf94596c755b70dfab
al-jabir <aljabir098@gmail.com>
2022-11-07 08:36:08 +0600

    404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from '../styles/notFuond.module.css';
+const PageNot = () => {
+  return (
+    <div>
+      <h1 className={styled.title}> 404 is page not found</h1>
+    </div>
+  );
+};
+
+export default PageNot;
